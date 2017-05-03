6910376fb49b585ad96d353826b67c798e9f8324
MelsenC <melsen.carlsburg@gmail.com>
2017-05-03 21:42:03 +0000

    styled the photos on the show page

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -39,6 +39,10 @@ class PictureUploader < CarrierWave::Uploader::Base
   # def extension_whitelist
   #   %w(jpg jpeg gif png)
   # end
+  
+  def content_type_whitelist
+    /image\//
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
